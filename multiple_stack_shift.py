f644d7f24722660e5c5c5c8dc88013d3d7debebc
sarvesh-nfer <sthapha@nference.net>
2022-05-26 15:35:58 +0530

    for varshini

--- multiple_stack_shift.py
+++ multiple_stack_shift.py
@@ -4,7 +4,7 @@ from plotly.subplots import make_subplots
 import glob
 import sqlite3
 
-txt = glob.glob("/home/adminspin/Music/scanner_MGB_data/46P_data/*.db")
+txt = glob.glob("/home/adminspin/Music/db_data_shipped/*.db")
 
 
 df = pd.DataFrame()
@@ -40,8 +40,13 @@ fig3.update_xaxes(title="Shift Values",dtick=1,tickangle=0,range=[-16.9,16.9])
 fig3.update_yaxes(title="Count")
 fig3.update_layout(width=1800,height=800)
 fig3.update_traces(textfont_size=20, textposition="outside",textangle=0, cliponaxis=True)
-fig3.add_annotation(text="<b>Stack Shift Data for <b>H01EBB46P-5403",xref="paper", yref="paper",showarrow=False,x=0, y=1.11,font=dict(family="Courier New, monospace",
+fig3.add_annotation(text="<b>Stack Shift Data across scanner <b>"+str(len(txt)),xref="paper", yref="paper",showarrow=False,x=0, y=1.11,font=dict(family="Courier New, monospace",
         size=24,color="RebeccaPurple"))
+
+for idx,i in enumerate(["yellow","blue","green"]):
+    fig3.add_vline(x=(df['x_shift'].mean() - (idx+1) * df['x_shift'].std()), line_width=3, line_dash="dash", line_color=i,row=1,col=1)
+    fig3.add_vline(x=(df['x_shift'].mean() + (idx+1) * df['x_shift'].std()), line_width=3, line_dash="dash", line_color=i,row=1,col=1)
+
 fig3.add_vline(x=-11.5, line_width=3, line_dash="dash", line_color="red")
 fig3.add_vline(x=11.5, line_width=3, line_dash="dash", line_color="red")
 
@@ -54,7 +59,7 @@ if round((len(df[(df['x_shift']<=12)&(df['x_shift']>=-12)])/len(df))*100,2) > 95
             borderwidth=2,borderpad=4,bgcolor="green",opacity=0.8,row=1,col=1)
 else:
     fig3.add_annotation(x=16,y=(df['x_shift'].value_counts().iloc[0]),xref="paper",yref="paper",
-        text="Percentage of distribution of X-Shift : "+str(round((len(df[(df['x_shift']<=12)&(df['x_shift']>=-12)])/len(df))*100,2)),
+        text="Percentage of distribution <br>of X-Shift : "+str(round((len(df[(df['x_shift']<=12)&(df['x_shift']>=-12)])/len(df))*100,2)),
         showarrow=True,font=dict(family="Courier New, monospace",size=16,color="#ffffff"),align="center",bordercolor="#c7c7c7",
         borderwidth=2,borderpad=4,bgcolor="crimson",opacity=0.8,row=1,col=1)
 
